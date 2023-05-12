4cad6ebea17aa796d62027b4c1d292244186851d
Jakub <kontakt@jnowak.eu>
2023-05-12 18:34:11 +0200

    File loader

--- igc_analyzer/src/igc_analyzer.hpp
+++ igc_analyzer/src/igc_analyzer.hpp
@@ -1,6 +1,55 @@
+#include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
-void load_text_file(char uri[]){
+class FileReader {
+public:
+    FileReader(std::string filename) : filename_(filename) {}
 
-}
+    std::string read() {
+        std::ifstream file(filename_);
+        if (file.is_open()) {
+            std::string contents;
+            std::string line;
+            while (getline(file, line)) {
+                contents += line + '\n';
+            }
+            file.close();
+            return contents;
+        } else {
+            std::cerr << "Error: Could not open file " << filename_ << std::endl;
+            return "";
+        }
+    }
 
+    std::vector<std::string> readLines() {
+        std::ifstream file(filename_);
+        std::vector<std::string> lines;
+        std::string line;
+        while (std::getline(file, line)) {
+            lines.push_back(line);
+        }
+        file.close();
+        return lines;
+    }
+
+    std::string readline() {
+        std::ifstream file(filename_);
+        if (file.is_open()) {
+            std::string contents;
+            std::string line;
+            while (getline(file, line)) {
+                contents += line + '\n';
+            }
+            file.close();
+            return contents;
+        } else {
+            std::cerr << "Error: Could not open file " << filename_ << std::endl;
+            return "";
+        }
+    }
+
+private:
+    std::string filename_;
+};
--- igc_analyzer/tests/test.cpp
+++ igc_analyzer/tests/test.cpp
@@ -2,11 +2,39 @@
 #include "CppUTestExt/MockSupport.h"
 #include "igc_analyzer.hpp"
 
-TEST_GROUP(FileLoad)
-{
+TEST_GROUP(FileReaderTestGroup) {
+    void setup() override {
+        // Create a temporary file with some contents
+        std::ofstream file("testfile.txt");
+        file << "This is a test file\n"
+             << "With multiple lines\n"
+             << "And some special characters: !@#$%^&*()\n";
+        file.close();
+    }
+
+    void teardown() override {
+        // Remove the temporary file
+        std::remove("testfile.txt");
+    }
 };
 
-TEST(FileLoad, FirstTest)
+TEST(FileReaderTestGroup, ReadsFileContents)
 {
-   load_text_file("test");
+   FileReader reader("testfile.txt");
+   std::string contents = reader.read();
+   STRCMP_EQUAL("This is a test file\nWith multiple lines\nAnd some special characters: !@#$%^&*()\n", contents.c_str());
+}
+
+TEST(FileReaderTestGroup, HandlesNonexistentFile) {
+    FileReader reader("nonexistent.txt");
+    std::string contents = reader.read();
+    STRCMP_EQUAL("", contents.c_str());
+}
+
+TEST(FileReaderTestGroup, ReadsLines) {
+    FileReader reader("testfile.txt");
+    auto lines = reader.readLines();
+    STRCMP_EQUAL("This is a test file", lines[0].c_str());
+    STRCMP_EQUAL("With multiple lines", lines[1].c_str());
+    STRCMP_EQUAL("And some special characters: !@#$%^&*()", lines[2].c_str());
 }
